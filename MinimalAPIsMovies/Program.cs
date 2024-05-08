1b3aff9931e3bfd126af300b8df8200c865c8a90
Ulises Cadena <ucadena07@gmail.com>
2024-05-08 06:58:27 -0500

    output cashed

--- MinimalAPIsMovies/Program.cs
+++ MinimalAPIsMovies/Program.cs
@@ -3,7 +3,7 @@ using MinimalAPIsMovies.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+//Services zone begins
 
 builder.Services.AddCors(opt =>
 {
@@ -17,18 +17,22 @@ builder.Services.AddCors(opt =>
         conf.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
+
+builder.Services.AddOutputCache();
+
+//Services zone ends
 var app = builder.Build();
 
 
 //Middleware zone begin
 app.UseCors();
-
+app.UseOutputCache();
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/genres",[EnableCors(policyName:"free")] () =>
 {
     return new List<Genre>() { new Genre() {Id = 1, Name = "Drama" }, new Genre() { Id = 2, Name = "Action" }, new Genre() { Id = 3, Name = "Comedy" } };   
-});
+}).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(15)));
 //Middleware zone ends
 app.Run();
